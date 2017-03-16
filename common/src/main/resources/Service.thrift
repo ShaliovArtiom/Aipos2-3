27bc2443612cd54d407b3dc6427289f809addf03
ShaliovArtiom <shaliov.artiom@mail.ru>
2017-03-16 21:20:57 +0300

    исправлено общее

--- common/src/main/resources/Service.thrift
+++ common/src/main/resources/Service.thrift
@@ -2,7 +2,7 @@ namespace java by.bsuir.Shaliov.common.service
 
 include "Models.thrift"
 
-service StudentService {
+service BookService {
     void addBookTable( 1: i32 id, 2: string bookName, 3: string authorName, 4: i32 pageValue),
     void renameBookTable( 1: i32 id, 2: string bookName, 3: string authorName, 4: i32 pageValue),
     void deleteBookTable(1: i32 id, 2: string bookName, 3: string authorName, 4: i32 pageValue),
